9480f51b533afb28d747297f1dcfa0dd2e39565c
Guruprasad J Rao <kshkrao3@gmail.com>
2023-03-24 22:34:22 +0530

    Changes to the endpoint

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5003/', {
+  const response = await fetch('https://codex-sgws.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
